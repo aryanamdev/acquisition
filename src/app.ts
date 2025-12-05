92ea5933ba22736cf28c118441b7dcc96935d096
aryanamdev <aryanamdev08@gmail.com>
2025-12-05 15:55:14 +0530

    feat: server setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
-import express from "express"
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Hello world!",
-        data: null,
-    }).status(200)
-})
+app.get('/', (req, res) => {
+  res
+    .json({
+      message: 'Hello world!',
+      data: null,
+    })
+    .status(200);
+});
 
-
-export default app
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
-import dotenv from "dotenv/config"
+import app from './app.js';
+import config from './config/config.js';
 
-import app from "./app.js"
-import config from "./config/config.js"
-
-app.listen(process.env.PORT || 3000, (err) => {
-    if(err){
-        console.error({
-            message: err.message,
-            cause: err.cause,
-            trace: err.stack
-        })
-    }
-    console.log(`app is listening on port ${config.port}`)
-})
+app.listen(process.env.PORT || 3000, err => {
+  if (err) {
+    console.error({
+      message: err.message,
+      cause: err.cause,
+      trace: err.stack,
+    });
+  }
+  console.log(`app is listening on port ${config.port}`);
+});
